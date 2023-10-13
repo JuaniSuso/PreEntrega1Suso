35dfec1846a49516d8466287456499cabfdd9706
Juan Suso <juanisuso@gmail.com>
2023-10-12 23:39:56 -0300

    Added navbar

--- suso-ecomerce/src/App.jsx
+++ suso-ecomerce/src/App.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
 
-import './App.css'
+
+//import './App.css'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/NavBar/NavBar'
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   
